51aba363144f7c5db099f8d0cb3c85af69534ec7
Miguel Vazquez <migueldv90@gmail.com>
2025-11-06 23:16:42 -0600

    Add UploadFileSVG method to StorageClientGCP for uploading SVG files to GCP storage, enhancing file upload capabilities.

---
+++ api-v1/utils/files/gcp-options.go
@@ -0,0 +1,28 @@
+package files
+
+import (
+	"bytes"
+	"context"
+	"io"
+	"time"
+)
+
+func (client *StorageClientGCP) UploadFileSVG(path string, content []byte) (string, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*60)
+	defer cancel()
+
+	writer := client.storageClient.Bucket(client.bucket).Object(path).NewWriter(ctx)
+	writer.ContentType = "image/svg+xml"
+	reader := bytes.NewBuffer(content)
+
+	if _, err := io.Copy(writer, reader); err != nil {
+		return "", err
+	}
+
+	if err := writer.Close(); err != nil {
+		return "", err
+	}
+
+	url := UrlGCP + client.bucket + "/" + path
+	return url, nil
+}
