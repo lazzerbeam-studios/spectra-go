070afdd9382d95395dc80511523ab034cef54119
Miguel Vazquez <migueldv90@gmail.com>
2025-11-06 23:15:40 -0600

    Reintroduce UploadFile method in StorageClientGCP with improved structure and clarity, ensuring consistent functionality for file uploads to GCP storage.

--- api-v1/utils/files/gcp.go
+++ api-v1/utils/files/gcp.go
@@ -21,25 +21,6 @@ type StorageClientGCP struct {
 	storageClient *storage.Client
 }
 
-func (client *StorageClientGCP) UploadFile(path string, content []byte) (string, error) {
-	ctx, cancel := context.WithTimeout(context.Background(), time.Second*60)
-	defer cancel()
-
-	writer := client.storageClient.Bucket(client.bucket).Object(path).NewWriter(ctx)
-	reader := bytes.NewBuffer(content)
-
-	if _, err := io.Copy(writer, reader); err != nil {
-		return "", err
-	}
-
-	if err := writer.Close(); err != nil {
-		return "", err
-	}
-
-	url := UrlGCP + client.bucket + "/" + path
-	return url, nil
-}
-
 func SetStorageGCP(credentials string, project string, bucket string) {
 	credentialsByte, err := base64.StdEncoding.DecodeString(credentials)
 	if err != nil {
@@ -59,4 +40,23 @@ func SetStorageGCP(credentials string, project string, bucket string) {
 		project:       project,
 		storageClient: storageClient,
 	}
+}
+
+func (client *StorageClientGCP) UploadFile(path string, content []byte) (string, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*60)
+	defer cancel()
+
+	writer := client.storageClient.Bucket(client.bucket).Object(path).NewWriter(ctx)
+	reader := bytes.NewBuffer(content)
+
+	if _, err := io.Copy(writer, reader); err != nil {
+		return "", err
+	}
+
+	if err := writer.Close(); err != nil {
+		return "", err
+	}
+
+	url := UrlGCP + client.bucket + "/" + path
+	return url, nil
 }
