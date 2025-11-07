361e47d9192316d60359440cf198d5dd8944832d
Miguel Vazquez <migueldv90@gmail.com>
2025-11-06 23:19:27 -0600

    Refactor ProfileUpdateEmailAPI to streamline authentication and email update logic, replacing manual checks with utility functions for improved clarity and error handling.

--- api-v1/routes/users_api/op-ProfileUpdateEmailAPI.go
+++ api-v1/routes/users_api/op-ProfileUpdateEmailAPI.go
@@ -2,7 +2,6 @@ package users_api
 
 import (
 	"context"
-	"encoding/json"
 	"strings"
 
 	"github.com/danielgtaylor/huma/v2"
@@ -12,38 +11,26 @@ import (
 	"api-go/models"
 	"api-go/utils/auth"
 	"api-go/utils/db"
-	"api-go/utils/encode"
 )
 
 type ProfileUpdateEmailInput struct {
-	Auth string `header:"Authorization"`
+	auth.AuthParam
 	Body struct {
-		Object models.ProfileUpdateEmail `json:"object"`
+		models.ProfileUpdateEmail
 	}
 }
 
 func (input *ProfileUpdateEmailInput) Resolve(ctx huma.Context) []error {
-	authID, authValid := auth.GetJWT(input.Auth)
-	if !authValid {
+	var err error
+	input.User, err = auth.AuthUser(input.Auth)
+	if err != nil {
 		return []error{huma.Error401Unauthorized("Unable to authenticate.")}
 	}
-	if authID != input.Body.Object.ID {
-		return []error{huma.Error401Unauthorized("User doesn't have permission.")}
-	}
 
-	profileObj, err := db.EntDB.User.Query().Where(user.ID(input.Body.Object.ID)).Only(context.Background())
-	if err != nil {
-		return []error{huma.Error404NotFound("User not found.")}
-	}
-
-	input.Body.Object.Email = strings.ToLower(input.Body.Object.Email)
-	if input.Body.Object.Email == profileObj.Email {
+	input.Body.Email = strings.ToLower(input.Body.Email)
+	usersList, err := db.EntDB.User.Query().Where(user.EmailEqualFold(input.Body.Email)).All(context.Background())
+	if len(usersList) > 0 || err != nil {
 		return []error{huma.Error400BadRequest("This email is not available.")}
-	} else {
-		usersEmail, err := db.EntDB.User.Query().Where(user.EmailEqualFold(input.Body.Object.Email)).All(context.Background())
-		if len(usersEmail) >= 1 || err != nil {
-			return []error{huma.Error400BadRequest("This email is not available.")}
-		}
 	}
 
 	return nil
@@ -56,18 +43,11 @@ type ProfileUpdateEmailOutput struct {
 }
 
 func ProfileUpdateEmailAPI(ctx context.Context, input *ProfileUpdateEmailInput) (*ProfileUpdateEmailOutput, error) {
-	userJson, _ := json.Marshal(input.Body.Object)
-	userMap := encode.JsonToMap(string(userJson))
-	delete(userMap, "id")
-
-	_, err := db.BunDB.NewUpdate().Model(&userMap).TableExpr("users").Where("id=?", input.Body.Object.ID).Exec(ctx)
+	profileObj, err := input.User.Update().
+		SetEmail(input.Body.Email).
+		Save(ctx)
 	if err != nil {
-		return nil, huma.Error400BadRequest("Unable to update profile.")
-	}
-
-	profileObj, err := db.EntDB.User.Query().Where(user.ID(input.Body.Object.ID)).Only(ctx)
-	if err != nil {
-		return nil, huma.Error404NotFound("User not found.")
+		return nil, huma.Error400BadRequest("Unable to update user.")
 	}
 
 	response := &ProfileUpdateEmailOutput{}
