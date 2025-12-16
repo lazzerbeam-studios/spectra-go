1cdc9d1661a711d4bb264de27953bb3d1847f727
Miguel Vazquez <migueldv90@gmail.com>
2025-12-15 22:48:06 -0600

    Update Docker image tags to version 1765860480 in build scripts and configuration files for consistency.

--- docker_buildx.sh
+++ docker_buildx.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker buildx build --file Dockerfile . --tag ghcr.io/lazzerbeam-studios/spectra:ute1765858665 --platform linux/amd64,linux/arm64 --push
+docker buildx build --file Dockerfile . --tag ghcr.io/lazzerbeam-studios/spectra:ute1765860480 --platform linux/amd64,linux/arm64 --push
