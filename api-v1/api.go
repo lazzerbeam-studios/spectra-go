03f14018613e26ecd1f43a2be33ed56ca752fd91
Miguel Vazquez <migueldv90@gmail.com>
2026-01-28 16:32:43 -0600

    Comment out Google Maps and GCP file client initialization in main function to simplify configuration.

--- api-v1/api.go
+++ api-v1/api.go
@@ -17,11 +17,9 @@ import (
 	"api-go/routes/auth_api"
 	"api-go/routes/home_api"
 	"api-go/routes/users_api"
-	"api-go/scripts/places"
 	"api-go/utils/auth"
 	"api-go/utils/cache"
 	"api-go/utils/db"
-	"api-go/utils/files"
 )
 
 type OptionsCLI struct {
@@ -42,8 +40,8 @@ func main() {
 		db.SetEntDB(cfg.Database)
 		auth.SetSecret(cfg.Secret)
 		cache.SetClient(cfg.Valkey)
-		places.SetClient(cfg.Google_Maps_API_Key)
-		files.SetClientGCP(cfg.Google_Credentials, cfg.Google_Project, cfg.Google_Bucket)
+		// places.SetClient(cfg.Google_Maps_API_Key)
+		// files.SetClientGCP(cfg.Google_Credentials, cfg.Google_Project, cfg.Google_Bucket)
 
 		mutations.UserHook()
 
