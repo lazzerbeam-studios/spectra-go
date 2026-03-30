6986c77c2f3d9448477550ffeeec1dd40febaed7
Miguel Vazquez <migueldv90@gmail.com>
2026-03-30 10:21:16 -0500

    Refactor environment configuration for improved readability and consistency in key definitions

--- api-v1/environment/environment.go
+++ api-v1/environment/environment.go
@@ -3,27 +3,21 @@ package environment
 import "github.com/spf13/viper"
 
 type EnvironmentConfiguration struct {
-	Secret string `mapstructure:"SECRET"`
-
-	Database string `mapstructure:"DATABASE"`
-
-	Valkey string `mapstructure:"VALKEY"`
-
-	GoogleBucket      string `mapstructure:"GOOGLE_BUCKET"`
-	GoogleProject     string `mapstructure:"GOOGLE_PROJECT"`
-	GoogleCredentials string `mapstructure:"GOOGLE_CREDENTIALS"`
-	GoogleMapsAPIKey  string `mapstructure:"GOOGLE_MAPS_API_KEY"`
-
-	SupabaseSecret string `mapstructure:"SUPABASE_SECRET"`
-	SupabaseIssuer string `mapstructure:"SUPABASE_ISSUER"`
-
+	Secret              string `mapstructure:"SECRET"`
+	Database            string `mapstructure:"DATABASE"`
+	Valkey              string `mapstructure:"VALKEY"`
+	GoogleBucket        string `mapstructure:"GOOGLE_BUCKET"`
+	GoogleProject       string `mapstructure:"GOOGLE_PROJECT"`
+	GoogleCredentials   string `mapstructure:"GOOGLE_CREDENTIALS"`
+	GoogleMapsAPIKey    string `mapstructure:"GOOGLE_MAPS_API_KEY"`
+	SupabaseSecret      string `mapstructure:"SUPABASE_SECRET"`
+	SupabaseIssuer      string `mapstructure:"SUPABASE_ISSUER"`
 	BunnyVideoKey       string `mapstructure:"BUNNY_VIDEO_KEY"`
 	BunnyVideoLibraryID string `mapstructure:"BUNNY_VIDEO_LIBRARY_ID"`
-
-	BunnyStorageKey    string `mapstructure:"BUNNY_STORAGE_KEY"`
-	BunnyStorageZone   string `mapstructure:"BUNNY_STORAGE_ZONE"`
-	BunnyStorageRegion string `mapstructure:"BUNNY_STORAGE_REGION"`
-	BunnyStorageCDN    string `mapstructure:"BUNNY_STORAGE_CDN"`
+	BunnyStorageKey     string `mapstructure:"BUNNY_STORAGE_KEY"`
+	BunnyStorageZone    string `mapstructure:"BUNNY_STORAGE_ZONE"`
+	BunnyStorageRegion  string `mapstructure:"BUNNY_STORAGE_REGION"`
+	BunnyStorageCDN     string `mapstructure:"BUNNY_STORAGE_CDN"`
 }
 
 func SetEnvironment(env string) (cfg EnvironmentConfiguration, err error) {
